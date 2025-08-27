e607ef9e7a59ad2a27e465ac8280f0d8bec67dfc
FizzyApple12 <github@fizzyapple12.com>
2025-08-27 17:00:49 -0400

    add agenix secrets.nix

---
+++ secrets.nix
@@ -0,0 +1,14 @@
+{
+  pkgs
+}:
+let
+  publicKeys = let
+      authorizedKeys = pkgs.fetchurl {
+        url = "https://github.com/fizzyapple12.keys";
+        sha256 = "sha256-kHwxnznq7LHRoV1KCWhdkVwW65+AQzDTwMxd9a2S4zk=";
+      };
+    in pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
+in
+{
+  "secrets/age-files/cfAPIToken.age".publicKeys = publicKeys;
+}
