09a73e95926d0324012525390b358547595529ef
FizzyApple12 <github@fizzyapple12.com>
2025-08-27 17:45:39 -0400

    agenix secrets file fixes

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,10 @@
-{
-  pkgs,
-  ...
-}:
 let
-  publicKeys = let
-      authorizedKeys = pkgs.fetchurl {
+  publicKeys =  let
+      authorizedKeys = import <nix/fetchUrl> {
         url = "https://github.com/fizzyapple12.keys";
         sha256 = "sha256-kHwxnznq7LHRoV1KCWhdkVwW65+AQzDTwMxd9a2S4zk=";
       };
-    in pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
+    in import <nix/lib/splitString> "\n" (builtins.readFile authorizedKeys);
 in
 {
   "secrets/age-files/cfAPIToken.age".publicKeys = publicKeys;
