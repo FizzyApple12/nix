bebe967308f0fffe15a7e36c79a9263d60fa3054
FizzyApple12 <github@fizzyapple12.com>
2025-08-27 18:28:25 -0400

    update keys hash

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,7 @@ let
       pkgs = import <nixpkgs> {};
       authorizedKeys = pkgs.fetchurl {
         url = "https://github.com/fizzyapple12.keys";
-        sha256 = "sha256-kHwxnznq7LHRoV1KCWhdkVwW65+AQzDTwMxd9a2S4zk=";
+        sha256 = "sha256-n8CVlzwDy+wBA+3fwWQuKBNL69tIcxyaSiNCbgoRnuQ=";
       };
     in pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
 in
