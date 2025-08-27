8f2ae7c85a2e7ec8283b7c72322a0dc1c6cb9b50
FizzyApple12 <github@fizzyapple12.com>
2025-08-27 06:02:54 -0400

    fix conflict

--- servers/nginx-reverse-proxy/default.nix
+++ servers/nginx-reverse-proxy/default.nix
@@ -41,7 +41,7 @@
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
+      PermitRootLogin = "prohibit-password";
       AllowUsers = [ "fizzyapple12" ];
     };
   };
