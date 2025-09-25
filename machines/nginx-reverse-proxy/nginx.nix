f95281a59c0adaf0d095c2ad1678e040380b37e6
FizzyApple12 <github@fizzyapple12.com>
2025-09-25 13:20:03 -0400

    change port to use proper reverse proxy setup from the nextcloud aio documentation

--- machines/nginx-reverse-proxy/nginx.nix
+++ machines/nginx-reverse-proxy/nginx.nix
@@ -107,7 +107,7 @@
           enableACME = true;
           forceSSL = true;
           locations."/" = {
-            proxyPass = "https://100.74.252.69:80";
+            proxyPass = "http://100.74.252.69:11000$request_uri";
             recommendedProxySettings = true;
             proxyWebsockets = true;
           };
