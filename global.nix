8e036288a49df023a5a4855c3578365ef444a940
FizzyApple12 <github@fizzyapple12.com>
2025-12-12 02:37:46 -0500

    add host key to agenix paths

--- global.nix
+++ global.nix
@@ -60,7 +60,7 @@
 
   # janky hack to fix agenix
   age = {
-    identityPaths = ["/root/.ssh/id_ed25519" "/home/fizzyapple12/.ssh/id_ed25519"];
+    identityPaths = ["/etc/ssh/ssh_host_ed25519_key" "/root/.ssh/id_ed25519" "/home/fizzyapple12/.ssh/id_ed25519"];
     rekey = {
       masterIdentities = [
         /home/fizzyapple12/.ssh/id_ed25519.pub
