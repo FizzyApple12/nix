7e91cb44c61973c0c45a851d6735087bd4b9322b
FizzyApple12 <github@fizzyapple12.com>
2025-12-16 03:20:30 -0500

    switch hydra to a custom fork

--- machines/hydra/hydra.nix
+++ machines/hydra/hydra.nix
@@ -27,9 +27,7 @@
 
   services = {
     hydra = {
-      package = inputs.hydra.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (finalAttrs: previousAttrs: {
-        postgresql_13 = pkgs.postgresql;
-      });
+      package = inputs.hydra.packages.${pkgs.stdenv.hostPlatform.system}.default;
       enable = true;
       port = 3000;
       hydraURL = "https://hydra.fizzyapple12.com";
