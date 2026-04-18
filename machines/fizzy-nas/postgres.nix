76458d87e861550f217678b386413ab3d9f454b9
FizzyApple12 <github@fizzyapple12.com>
2026-04-18 01:59:14 -0400

    add default postgres superuser

--- machines/fizzy-nas/postgres.nix
+++ machines/fizzy-nas/postgres.nix
@@ -9,6 +9,16 @@
       package = pkgs.postgresql_17;
       dataDir = "/storage/postgres/17";
       ensureDatabases = [];
+      ensureUsers = [
+        {
+          name = "postgres";
+          ensureClasses = {
+            superuser = true;
+            createrole = true;
+            createdb = true;
+          };
+        }
+      ];
     };
   };
 }
