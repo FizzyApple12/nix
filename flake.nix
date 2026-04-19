8ff4c0fa14831051fedb415a1815f85cc965d73a
FizzyApple12 <github@fizzyapple12.com>
2026-04-19 00:45:51 -0400

    revert authentik to old version to perform upgrade

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     helium.url = "github:AlvaroParker/helium-nix";
     helium.inputs.nixpkgs.follows = "nixpkgs";
 
-    authentik-nix.url = "github:nix-community/authentik-nix";
+    authentik-nix.url = "github:nix-community/authentik-nix/8048437d601f772be45e10495d975cf0ac4acbf7";
     authentik-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
