5baee1bec3c0f6634ab149517b996eb97369c4a6
FizzyApple12 <github@fizzyapple12.com>
2026-04-21 19:47:08 -0400

    hm that didn't work

--- machines/nginx-reverse-proxy/nginx.nix
+++ machines/nginx-reverse-proxy/nginx.nix
@@ -88,7 +88,7 @@
           forceSSL = true;
           locations = {
             "/" = {
-              proxyPass = "http://100.117.151.76:80";
+              proxyPass = "http://100.117.151.76";
               recommendedProxySettings = true;
               proxyWebsockets = true;
             };
