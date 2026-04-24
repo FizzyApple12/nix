82bfbb8e2d3f9aedd0bb3e1804628a500a701acb
FizzyApple12 <github@fizzyapple12.com>
2026-04-24 17:46:04 -0400

    switch radicale back to ldap

--- machines/fizzy-nas/radicale.nix
+++ machines/fizzy-nas/radicale.nix
@@ -1,6 +1,7 @@
 {
   config,
   inputs,
+  pkgs,
   ...
 }: {
   nixpkgs = {
@@ -37,17 +38,13 @@
           ];
         };
         auth = {
-          # type = "ldap";
-          # ldap_uri = "ldap://auth.fizzyapple12.com:389";
-          # ldap_base = "dc=auth,dc=fizzyapple12,dc=com";
-          # ldap_reader_dn = "cn=radicale,ou=users,DC=auth,DC=fizzyapple12,DC=com";
-          # ldap_secret_file = config.age.secrets.radicale-secret.path;
-          # ldap_filter = "(&(objectClass=user)(cn={0}))";
-          # lc_username = true;
-          type = "oauth2";
-          oauth2_token_endpoint = "https://auth.fizzyapple12.com/application/o/token/";
-          oauth2_client_id = "YkJ68p8n45TegKGYzcRCjbD3b2l2i68tbWNKODH7";
-          oauth2_client_secret = config.age.secrets.radicale-secret.path;
+          type = "ldap";
+          ldap_uri = "ldap://auth.fizzyapple12.com:389";
+          ldap_base = "dc=auth,dc=fizzyapple12,dc=com";
+          ldap_reader_dn = "cn=radicale,ou=users,DC=auth,DC=fizzyapple12,DC=com";
+          ldap_secret_file = config.age.secrets.radicale-secret.path;
+          ldap_filter = "(&(objectClass=user)(cn={0}))";
+          lc_username = true;
         };
         storage = {
           filesystem_folder = "/storage/radicale";
