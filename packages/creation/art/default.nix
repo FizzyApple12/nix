88d18b78a5554fba4b9d07266e34df1dba5c8af5
FizzyApple12 <github@fizzyapple12.com>
2026-06-01 20:24:22 -0400

    re-enable rocm support for blender

--- packages/creation/art/default.nix
+++ packages/creation/art/default.nix
@@ -3,7 +3,11 @@
     systemPackages = [
       pkgs.inkscape
       pkgs.krita
-      pkgs.blender
+      # pkgs.blender
+      (pkgs.blender.override {
+        config.rocmSupport = true;
+        config.cudaSupport = false;
+      })
     ];
   };
 }
