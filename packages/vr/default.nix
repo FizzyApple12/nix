347bbc89204501ebc893bd481a8bcae6cdb1854b
FizzyApple12 <github@fizzyapple12.com>
2026-06-09 17:33:48 -0400

    make setcap fixing service for steamvr

--- packages/vr/default.nix
+++ packages/vr/default.nix
@@ -17,4 +17,29 @@
       pkgs.lighthouse-steamvr
     ];
   };
+
+  systemd = {
+    services = {
+      "steamvr-setcap" = {
+        description = "Re-apply CAP_SYS_NICE to SteamVR vrcompositor-launcher";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = let
+            launcher = "/home/fizzyapple12/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher";
+          in "${pkgs.bash}/bin/bash -c '[ -f ${launcher} ] && ${pkgs.libcap}/bin/setcap CAP_SYS_NICE+ep ${launcher}'";
+        };
+      };
+    };
+
+    paths = {
+      "steamvr-setcap" = {
+        description = "Watch for SteamVR binary changes";
+        pathConfig = {
+          PathModified = "/home/fizzyapple12/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher";
+          Unit = "steamvr-setcap.service";
+        };
+        wantedBy = ["multi-user.target"];
+      };
+    };
+  };
 }
