a613b24fedfc37f61d169e4261fa3478167f4e19
FizzyApple12 <github@fizzyapple12.com>
2026-06-09 23:10:47 -0400

    add more plugins for obs

--- packages/creation/video/streaming.nix
+++ packages/creation/video/streaming.nix
@@ -22,10 +22,14 @@
       pkgs.ndi-6
       (pkgs.wrapOBS {
         plugins = [
+          pkgs.obs-studio-plugins.obs-vaapi
+          pkgs.obs-studio-plugins.obs-noise
           pkgs.obs-studio-plugins.distroav
           pkgs.obs-studio-plugins.wlrobs
           pkgs.obs-studio-plugins.obs-pipewire-audio-capture
           pkgs.obs-studio-plugins.obs-shaderfilter
+          pkgs.obs-studio-plugins.obs-composite-blur
+          pkgs.obs-studio-plugins.obs-dvd-screensaver
         ];
       })
     ];
