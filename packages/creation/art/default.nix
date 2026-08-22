729539acdb0422fc850246cb68a91a34f854ec2c
FizzyApple12 <github@fizzyapple12.com>
2026-08-22 15:25:41 -0400

    finally fix rocm support in blender

--- packages/creation/art/default.nix
+++ packages/creation/art/default.nix
@@ -1,4 +1,21 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: {
+  nixpkgs = {
+    overlays = [
+      (
+        self: super: (
+          let
+            nixpkgs-master = import inputs.nixpkgs-master {
+              inherit (self) system;
+              config.allowUnfree = true;
+            };
+          in {
+            blender = nixpkgs-master.pkgsRocm.blender;
+          }
+        )
+      )
+    ];
+  };
+
   environment = {
     systemPackages = [
       pkgs.inkscape
